15f3904486bf8c3895270c57d861c5307095c98b
배용표 <yp.bae@mtov.net>
2019-01-26 02:18:54 +0900

    Function Study

---
+++ FunctionStudy.dart
@@ -0,0 +1,12 @@
+main(List<String> args) {
+
+  print(numReturn(11));
+  print(numReturnHO(22));
+  
+}
+
+int numReturn(int tempNumber) {
+  return tempNumber;
+}
+
+int numReturnHO(tempNumber) => tempNumber;

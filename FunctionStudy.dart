3cafc3c1a83bf0e4483db2c64c55216819fc3ca5
배용표 <yp.bae@mtov.net>
2019-01-26 03:20:30 +0900

    Function Study

--- FunctionStudy.dart
+++ FunctionStudy.dart
@@ -2,6 +2,11 @@ main(List<String> args) {
 
   print(numReturn(11));
   print(numReturnHO(22));
+
+  OptionalParamer("key");
+  OptionalParamer("key", "value");
+
+  print(defaultParameter());
   
 }
 
@@ -9,4 +14,11 @@ int numReturn(int tempNumber) {
   return tempNumber;
 }
 
-int numReturnHO(tempNumber) => tempNumber;
+int numReturnHO(int tempNumber) => tempNumber;
+
+// Optional Positional
+void OptionalParamer( String key,[ String value]) =>{};
+
+//defualt Parameter
+String defaultParameter({String key = "Default" }) => key;
+
